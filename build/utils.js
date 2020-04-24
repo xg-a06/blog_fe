33362bbe606883d30654ba3358b302988dae243e
x.g <x.g@xgdeiMac.local>
2020-04-24 11:34:42 +0800

    初始化个人博客前端项目

---
+++ build/utils.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const config = require('./config')
+
+function resolve (dir) {
+  return path.resolve(__dirname, '..', dir)
+}
+
+function subDir (dir) {
+  return path.posix.join(config[process.env.BUILD_ENV].SUB_DIR, dir)
+}
+
+function getCssLoaders () {
+  const env = process.env.NODE_ENV;
+  const build = process.env.BUILD_ENV;
+  let isLocal = build === 'local'
+  let isProd = env === 'production'
+  let sourceMap = !isProd
+  let lastLoader = isLocal ? 'style-loader' : MiniCssExtractPlugin.loader
+  let lastLoaderOptions = isLocal ? {} : { sourceMap: sourceMap }
+  const cssInclude = [/src/]
+  const loaders = [
+    {
+      test: /\.global\.css$/,
+      use: [
+        { loader: lastLoader },
+        {
+          loader: 'css-loader'
+        },
+        {
+          loader: 'postcss-loader'
+        }
+      ]
+    },
+    {
+      test: /^(?!.*\.global).*\.css$/,
+      use: [
+        { loader: lastLoader, options: lastLoaderOptions },
+        {
+          loader: 'css-loader',
+          options: { modules: { localIdentName: '[hash:base64:6]' }, sourceMap: sourceMap, importLoaders: 1 }
+        },
+        { loader: 'postcss-loader', options: { sourceMap: sourceMap } }
+      ],
+      include: cssInclude
+    }
+    , {
+        test: /\.global\.scss$/,
+        use: [
+          { loader: lastLoader, options: lastLoaderOptions },
+          {
+            loader: 'css-loader',
+            options: { sourceMap: sourceMap, importLoaders: 2 }
+          },
+          { loader: 'postcss-loader', options: { sourceMap: sourceMap } },
+          { loader: 'sass-loader', options: { sourceMap: sourceMap } }
+        ],
+        include: resolve('src')
+      },
+        {
+          test: /^(?!.*\.global).*\.scss$/,
+          use: [
+            { loader: lastLoader, options: lastLoaderOptions },
+            {
+              loader: 'css-loader',
+              options: { modules: { localIdentName: '[hash:base64:6]' }, sourceMap: sourceMap, importLoaders: 2 }
+            },
+            { loader: 'postcss-loader', options: { sourceMap: sourceMap } },
+            { loader: 'sass-loader', options: { sourceMap: sourceMap } }
+          ],
+          include: cssInclude
+        }
+              ]
+  return loaders
+}
+
+module.exports = {
+  resolve,
+  subDir,
+  getCssLoaders
+}
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import { version, Button } from 'antd';
+import './index.global.css';
+
+import App from './app';
+
+ReactDOM.render(<App />, document.getElementById('root'));
